0b17a1fce8419cf9ae4b77087549fadbb0446c06
Ilias Bissoufa <48054272+Iliasbis@users.noreply.github.com>
2025-06-30 21:55:05 +0100

    Deleted app.module.js.map

--- dist/modules/payments/dto/create-payment.dto.d.ts
+++
@@ -1,8 +0,0 @@
-import { PaymentMethod } from '../entities/payment.entity';
-export declare class CreatePaymentDto {
-    amount: number;
-    method: PaymentMethod;
-    bookingId: string;
-    stripePaymentIntentId?: string;
-    paypalOrderId?: string;
-}
